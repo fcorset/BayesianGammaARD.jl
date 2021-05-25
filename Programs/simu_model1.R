81dff930a1cd2139678707609f3912b1dfadb47c
fcorset <franck.corset@univ-grenoble-alpes.fr>
2021-05-25 10:55:33 +0200

    update simu 1 nouveau algo pour calcul loi stationnaire

--- Programs/simu_model1.R
+++ Programs/simu_model1.R
@@ -4,10 +4,10 @@
 rm(list=ls())
 library(ggplot2)
 library(tidyverse)
-tau = 0.2 # intervalle inter-inspection
+tau = 0.5 # intervalle inter-inspection
 rho = 0.5 # parametre ARDinf
-L=2 # seuil pour MP
-M=4 # seuil pout MC
+L=1 # seuil pour MP
+M=2 # seuil pout MC
 tps.final <-10 # fenêtre d'observation du processus
 
 id.newcycle <- FALSE # Initialisation du nb de cycle de renouvellement
@@ -16,7 +16,7 @@ set.seed(123)
 # Simulation d'un processus gamma jusqu'au temps final
 pas = 0.01 # pas de temps pour simuler le processus
 
-alpha = 1.2 # paramètre de forme de Gamma a = alpha (t)^beta
+alpha = 1 # paramètre de forme de Gamma a = alpha (t)^beta
 beta = 1 # paramètre de forme  de Gamma
 b=1   # paramètre d'échelle du Gamma
 temps = seq(from = 0,to = tps.final,by = pas)
@@ -106,7 +106,85 @@ abline(v=tau*(1:nb.inspections), lty =3, col = grey(0.1), lwd = 0.45)
 # Calcul de la loi stationnaire
 #####################################
 
-# se donner une grille pour les abscisses
+
+## update 20/05/2021
+## version discrétisée pour le calcul de la loi stationnaire
+
+# fonction ntrap : renvoie l'intégrale par la méthode des trapèzes
+ntrap <- function(abs,ord){
+  # Cette fonction renvoie un vecteur de taille nc (pour différentes valeurs de y)
+  # abscisse abs : vecteur de taille nl
+  # ordonnée ord : matrice de taille nc*nl
+  nl<- length(abs)
+  if (is.matrix(ord) == T){
+    nc <- nrow(ord)
+    res <- rowSums((matrix(1,ncol=1,nrow=nc)%*%diff(abs))*(ord[,-ncol(ord)]+ord[,-1])/2)
+    } else {
+    nc <- 1
+    res <- sum(diff(abs)*(ord[-length(ord)]+ord[-1])/2)
+    }
+  return(res)
+}
+
+
+
+
+
+# vecteur de l'état x
+level.x <- seq(0.01,M+1,0.01)
+n.level.x <-length(level.x)
+
+ind.L <- which( level.x == L)
+ind.M <- which( level.x == M)
+
+
+level.y <- seq(0.01,M+1,0.01)
+n.level.y <-length(level.y)
+
+mat.trans <- matrix(0,ncol=n.level.y,nrow = n.level.x)
+mat.trans.rho <- matrix(0,ncol=n.level.y,nrow = n.level.x)
+
+for(i in 1:(n.level.x-1)){
+  mat.trans[i,(i+1):n.level.y]<-dgamma(level.y[(i+1):n.level.y]-level.x[i],scale=b,shape = alpha*tau^beta)
+  mat.trans.rho[i,(i+1):n.level.y]<-dgamma(level.y[(i+1):n.level.y]-(1-rho)*level.x[i],scale=b,shape = alpha*tau^beta)
+}
+
+K<-200
+w<-matrix(0,nrow=K,ncol=n.level.y)
+w[1,]<-dgamma(level.y,scale = b,shape=alpha*tau^beta)
+
+
+pi<-list()
+pi[[1]]<-function(x) dgamma(x,scale = b,shape=alpha*tau^beta)
+
+
+for(k in 2:K){
+  
+  # Contribution à la première intégrale :
+  # Remplacer pi par la valeur ancienne de la loi stationnaire
+  
+  Q1 <- ntrap(level.x[1:ind.L],t(mat.trans[1:ind.L,]) * matrix(rep(w[k-1,1:ind.L],n.level.y),byrow = T,ncol = ind.L)) 
+  # Contribution à la deuxième intégrale :
+  # Remplacer pi par la valeur ancienne de la loi stationnaire
+  
+  Q2 <- ntrap(level.x[(ind.L+1):ind.M],t(mat.trans.rho[(ind.L+1):ind.M,]) *matrix(rep(w[k-1,(ind.L+1):ind.M],n.level.y),byrow = T,ncol = ind.M-ind.L) )
+  
+  # Contribution à la troisième intégrale :
+  # Remplacer pi par la valeur ancienne de la loi stationnaire
+  
+  Q3 <- dgamma(level.y,scale=b,shape=alpha*tau^beta) * ntrap(level.x[(ind.M+1):n.level.y],w[(ind.M+1):n.level.y])
+  
+  aux <- w[k,] <- Q1+Q2+Q3
+  fn_w <- function(x) {
+    res <- splinefun(x = level.x, y = aux)(x)*((0 < x) & (x<=max(level.x)))
+    return(res)
+  }
+  pi[[k]] <- fn_w
+  curve(pi[[k]](x), from = 0, to = max(level.x), lwd = 2, add = TRUE, col = k)
+}
+
+
+
 
 K<- 20 # nb itérations de l'algo de point fixe
 grid_abs <- seq(0.01,10,0.01)
@@ -114,7 +192,7 @@ nb_abs <- length(grid_abs)
 w<-matrix(nrow = K,ncol = nb_abs )
 
 pi<-list()
-pi[[1]]<-function(x) dexp(x)
+pi[[1]]<-function(x) dgamma(x,scale = b,shape=alpha*tau^beta)
 
 
 curve(pi[[1]](x), from = 0, to = max(grid_abs), lwd = 2)

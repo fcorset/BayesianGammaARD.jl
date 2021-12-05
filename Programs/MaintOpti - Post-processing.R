2d04483693c0dd803d19d37d0f8d640e9b495864
cparoissin <cparoiss@univ-pau.fr>
2021-12-05 12:26:44 +0100

    Rainy sunday

--- Programs/MaintOpti - Post-processing.R
+++ Programs/MaintOpti - Post-processing.R
@@ -14,7 +14,7 @@ fic.name <- paste("Results-cas-", NumCas, ".Rd", sep = "")
 load(file = fic.name)
 
 fig.ficname <- paste("../Figures/Results-cas-", NumCas, ".pdf", sep = "")
-#pdf(fig.ficname)
+pdf(fig.ficname)
 plot(x = output[[1]]$seq.L, y = output[[1]]$cout.L, type = "l", lwd = 3, xlab ="L", ylab = "Cost", main = "")
 
 cout.L.smooth <- loess(formula = cout.L ~ seq.L, span = 0.5, data = output[[1]])
@@ -26,7 +26,7 @@ idx <- which.min(yfit1)
 cat("Coût optimal : ", yfit1[idx], "\n")
 cat("L optimal : ", xfit[idx], "\n")
 
-#dev.off()
+dev.off()
 
 
 MatParam <-  read_excel("../Figures/res.xlsx")

fc3451b5e4b85f28f03f77d3de67dd86b88eb924
cparoissin <cparoiss@univ-pau.fr>
2021-12-10 09:04:07 +0100

    Update

--- Programs/Cluster UPPA - Resultats/MaintOpti - Post-processing.R
+++ Programs/Cluster UPPA - Resultats/MaintOpti - Post-processing.R
@@ -8,12 +8,12 @@ library(writexl)
 # 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48
 # 50 51
 
-NumCas <- 49
+NumCas <- 4
 fic.name <- paste("Results-cas-", NumCas, ".Rd", sep = "")
 
 load(file = fic.name)
 
-fig.ficname <- paste("../Figures/Results-cas-", NumCas, ".pdf", sep = "")
+fig.ficname <- paste("../../Figures2/Results-cas-", NumCas, ".pdf", sep = "")
 pdf(fig.ficname)
 plot(x = output[[1]]$seq.L, y = output[[1]]$cout.L, type = "l", lwd = 3, xlab ="L", ylab = "Cost", main = "")
 
@@ -29,10 +29,10 @@ cat("L optimal : ", xfit[idx], "\n")
 dev.off()
 
 
-MatParam <-  read_excel("../Figures/res.xlsx")
+MatParam <-  read_excel("../../Figures2/res.xlsx")
 MatParam$L_opt[NumCas] <- xfit[idx]
 MatParam$Cout_opt[NumCas] <- yfit1[idx]
 
-write_xlsx(x = MatParam, path = "../Figures/res.xlsx")
+write_xlsx(x = MatParam, path = "../../Figures2/res.xlsx")
 
 

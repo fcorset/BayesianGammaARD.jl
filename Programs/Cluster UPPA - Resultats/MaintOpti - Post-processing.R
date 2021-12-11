5b48a5f494f30dbe18abdca5dadab7e636f788cc
cparoissin <cparoiss@univ-pau.fr>
2021-12-11 09:59:11 +0100

    Update

--- Programs/Cluster UPPA - Resultats/MaintOpti - Post-processing.R
+++ Programs/Cluster UPPA - Resultats/MaintOpti - Post-processing.R
@@ -3,12 +3,7 @@
 library(readxl)
 library(writexl)
 
-# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-# 17 18 19 20 21 22 23 24 25 26
-# 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48
-# 50 51
-
-NumCas <- 6
+NumCas <- 13
 fic.name <- paste("Results-cas-", NumCas, ".Rd", sep = "")
 
 load(file = fic.name)
@@ -17,7 +12,8 @@ fig.ficname <- paste("../../Figures2/Results-cas-", NumCas, ".pdf", sep = "")
 pdf(fig.ficname)
 plot(x = output[[1]]$seq.L, y = output[[1]]$cout.L, type = "l", lwd = 3, xlab ="L", ylab = "Cost", main = "")
 
-cout.L.smooth <- loess(formula = cout.L ~ seq.L, span = 0.5, data = output[[1]])
+cout.L.smooth <- loess(formula = cout.L ~ seq.L, span = 0.75, data = output[[1]])
+
 
 xfit <- seq(from = min(output[[1]]$seq.L), to = max(output[[1]]$seq.L), by = 0.01)
 yfit1 <- predict(cout.L.smooth, newdata = xfit)

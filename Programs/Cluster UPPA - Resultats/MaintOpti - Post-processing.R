3ea6949362bb0f114792354ef2e974ff28819421
cparoissin <cparoiss@univ-pau.fr>
2021-12-13 19:52:47 +0100

    Update

--- Programs/Cluster UPPA - Resultats/MaintOpti - Post-processing.R
+++ Programs/Cluster UPPA - Resultats/MaintOpti - Post-processing.R
@@ -3,7 +3,7 @@
 library(readxl)
 library(writexl)
 
-NumCas <- 45
+NumCas <- 1
 
 fic.name <- paste("Results-cas-", NumCas, ".Rd", sep = "")
 
@@ -13,22 +13,21 @@ fig.ficname <- paste("../../Figures2/Results-cas-", NumCas, ".pdf", sep = "")
 pdf(fig.ficname)
 plot(x = output[[1]]$seq.L, y = output[[1]]$cout.L, type = "l", lwd = 3, xlab ="L", ylab = "Cost", main = "")
 
-cout.L.smooth <- loess(formula = cout.L ~ seq.L, span = 0.75, data = output[[1]])
+#cout.L.smooth <- loess(formula = cout.L ~ seq.L, span = 0.75, data = output[[1]])
+#xfit <- seq(from = min(output[[1]]$seq.L), to = max(output[[1]]$seq.L), by = 0.01)
+#yfit1 <- predict(cout.L.smooth, newdata = xfit)
+#lines(x = xfit, y = yfit1, col = "red", lwd = 3, xlab = "L")
 
-
-xfit <- seq(from = min(output[[1]]$seq.L), to = max(output[[1]]$seq.L), by = 0.01)
-yfit1 <- predict(cout.L.smooth, newdata = xfit)
-lines(x = xfit, y = yfit1, col = "red", lwd = 3, xlab = "L")
-idx <- which.min(yfit1)
-cat("Coût optimal : ", yfit1[idx], "\n")
-cat("L optimal : ", xfit[idx], "\n")
+idx <- which.min(output[[1]]$cout.L)
+cat("Coût optimal : ", output[[1]]$cout.L[idx], "\n")
+cat("L optimal : ", output[[1]]$seq.L[idx], "\n")
 
 dev.off()
 
 
 MatParam <-  read_excel("../../Figures2/res.xlsx")
-MatParam$L_opt[NumCas] <- xfit[idx]
-MatParam$Cout_opt[NumCas] <- yfit1[idx]
+MatParam$L_opt[NumCas] <- output[[1]]$seq.L[idx]
+MatParam$Cout_opt[NumCas] <- output[[1]]$cout.L[idx]
 
 write_xlsx(x = MatParam, path = "../../Figures2/res.xlsx")
 

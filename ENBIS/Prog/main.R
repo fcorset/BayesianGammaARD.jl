a4608207ecc5bdb424163b728ca35656544a72d3
fcorset <franck.corset@univ-grenoble-alpes.fr>
2022-05-19 09:29:23 +0200

    update EM

--- ENBIS/Prog/main.R
+++ ENBIS/Prog/main.R
@@ -11,13 +11,13 @@ library(tidyverse)
 
 tau <- 1 # intervalle inter-inspection
 rho <- 0.2 # parametre ARDinf pour les maintenances efficaces avec proba p
-rho_w <- 0.4 # parametre ARDinf pour les maintenances néfastes avec proba 1-p
-p <- 0.9 # proba que la maintenance préventive soit efficace
+rho_w <- 0.3 # parametre ARDinf pour les maintenances néfastes avec proba 1-p
+p <- 0.7 # proba que la maintenance préventive soit efficace
 
 
 L <- 5 # seuil pour MP
 M <- 10 # seuil pout MC
-tps.final <- 500 # fenêtre d'observation du processus
+tps.final <- 25 # fenêtre d'observation du processus
 
 id.newcycle <- FALSE # Initialisation du nb de cycle de renouvellement
 
@@ -128,6 +128,8 @@ plot(1:nb.inspections,obs,type="p",xlim = c(0,tps.final),xlab = "temps",ylab="D�
 abline(h=L,col="blue")
 abline(h=M,col="red")
 abline(v=tau*(1:nb.inspections), lty =3, col = grey(0.1), lwd = 0.45)
+abline(v=81)
+
 
 
 
@@ -138,53 +140,132 @@ abline(v=tau*(1:nb.inspections), lty =3, col = grey(0.1), lwd = 0.45)
 #####################################
 
 # initialisation des paramètres
-K<-100
+K<-50
 
 hat.theta<-matrix(nrow=K+1,ncol=6)
-hat.theta[1,] <- c(2,1.5,1.5,0.5,0.5,0.5) # (alpha,beta,b,rho,rho^\prime,p)
+hat.theta[1,] <- c(1.2,1.1,1.5,0.2,0.2,0.5) # (alpha,beta,b,rho,rho^\prime,p)
 
-delta <- hat.theta[1,1]*(temps.insp^hat.theta[1,2]-c(0,temps.insp[1:(nb.inspections-1)])^hat.theta[1,2])
-p.tilde <- (hat.theta[1,6]*dgamma(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[1,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[1,3]))/(hat.theta[1,6]*dgamma(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[1,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[1,3])+(1-hat.theta[1,6])*dgamma(obs-(1-c(0,Delta.P[(nb.inspections-1)]))*(1+hat.theta[1,5])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[1,3]))
+# On ne définit les p.tildes[i] que lorsque u_{i-1}=1 
+ind.u<-which(vect.u[1:(nb.inspections-1)]==1)
+
+# Indice intervenant dans les sommes (i.e tel que u_{i-1}=1)
+ind.i.u<-rep(0,nb.inspections)
+ind.i.u[ind.u+1] <- 1
+
+# indice i lorsque Delta_{i-1}=1
+ind.Delta<-which(Delta.P==1)
+
+ind.i.Delta<-rep(0,nb.inspections)
+ind.i.Delta[ind.Delta+1] <- 1
+
+#Définition des y_{t_{i-1}}
+obs.pre<-c(0,obs[1:(nb.inspections-1)])*(1-ind.i.Delta)
+
+temps.insp.pre <- c(0,temps.insp[1:(nb.inspections-1)])
+
+data <- data.frame(temps.insp,temps.insp.pre,obs,obs.pre,vect.u,vect.b,ind.i.u,Delta.P,ind.i.Delta)
+
+
+
+data1 <- filter(data,ind.i.u==1)
+#data0 <- filter(data,ind.i.u==0)
+
+delta <- hat.theta[1,1]*(data$temps.insp^hat.theta[1,2]-data$temps.insp.pre^hat.theta[1,2])
+
+
+
+  
+#p.tilde <- (hat.theta[1,6]*dgamma(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[1,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[1,3]))/(hat.theta[1,6]*dgamma(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[1,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[1,3])+(1-hat.theta[1,6])*dgamma(obs-(1-c(0,Delta.P[(nb.inspections-1)]))*(1+hat.theta[1,5])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[1,3]))
+
+# p.tilde uniquement défini lorsque u_{i-1}=1
+
+p.tilde <- ifelse(data1$obs/data1$obs.pre<=1,1,(hat.theta[1,6]*dgamma(data$obs[ind.i.u==1]-(1-hat.theta[1,4])*data$obs.pre[ind.i.u==1],delta[ind.i.u==1],rate=hat.theta[1,3]))/(hat.theta[1,6]*dgamma(data$obs[ind.i.u==1]-(1-hat.theta[1,4])*data$obs.pre[ind.i.u==1],delta[ind.i.u==1],rate=hat.theta[1,3])+(1-hat.theta[1,6])*dgamma(data$obs[ind.i.u==1]-(1+hat.theta[1,5])*data$obs.pre[ind.i.u==1],delta[ind.i.u==1],rate=hat.theta[1,3])))
+
+
+
+data.temp<-data.frame(data1,p.tilde)
+data1.rhow<-filter(data.temp,p.tilde<1)
+
+# Définition de la borne inf pour rho
+lower.rho <- max(1-data1$obs/data1$obs.pre)
+
+# Définition de la borne sup pour rhow en enlevant les cas où p.tilde = 1
+upper.rhow <- min(data1.rhow$obs/data1.rhow$obs.pre-1)
+
+
+
+
+#data1.rho<-filter(data1,comp.p.tilde>0)
+
+# forcer les p.tilde = 1 lorsque y_i/y_{i-1}<1 !
+
+# p.tilde[which(data1$obs/data1$obs.pre<=1)] <- 1
 
 for (k in 2:(K+1)){
   hat.theta[k,6]<-mean(p.tilde) # mise à jour de p
   # Calcul des delta_i
   num.b<-sum(hat.theta[k-1,1]*(temps.insp^hat.theta[k-1,2]-(c(0,temps.insp[1:(nb.inspections-1)]))^hat.theta[k-1,2]))
-  det.b<-sum(p.tilde*(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[k-1,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]))+(1-p.tilde)*(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1+hat.theta[k-1,5])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)])))
   
+  det.b.1 <- sum(data$obs[ind.i.u==0]-data$obs.pre[ind.i.u==0])
+  det.b.2 <- sum(p.tilde*(data$obs[ind.i.u==1]-(1-hat.theta[k-1,4])*data$obs.pre[ind.i.u==1])+(1-p.tilde)*(data$obs[ind.i.u==1]-(1+hat.theta[k-1,5])*data$obs.pre[ind.i.u==1]))
+ 
+  det.b<-det.b.1+det.b.2  
   hat.theta[k,3]<-num.b/det.b # mise à jour de b
   
   # Pour la mise à jour de rho et rho_w, on utilise optim car uniroot ne marche pas... 
   # En effet, plusieurs valeurs annulent la dérivée...
   
-  f.rho <- function(x){
-    y.rho <- obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-x[1])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)])
-    y.rhow <-obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1+x[2])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)])
+  f.rho <- Vectorize(function(x){
+    y.rho <- data$obs[ind.i.u==1] - (1-x)*data$obs.pre[ind.i.u==1]
+     
     logy.rho<-ifelse(y.rho<=0,-Inf,log(y.rho))
+    
+    return(-hat.theta[k,3]*sum(p.tilde*y.rho)+sum(p.tilde*(delta[ind.i.u==1]-1)*logy.rho))
+  })
+  hat.theta[k,4]<-optimize(f.rho,c(lower.rho,1),maximum = T)$maximum
+  
+  f.rhow <- Vectorize(function(x){
+    
+    y.rhow <-data1.rhow$obs - (1+x)*data1.rhow$obs.pre
     logy.rhow<-ifelse(y.rhow<=0,-Inf,log(y.rhow))
-    return(-hat.theta[k,3]*(sum(p.tilde*y.rho+(1-p.tilde)*y.rhow))+sum(p.tilde*(delta-1)*logy.rho+(1-p.tilde)*(delta-1)*ifelse(p.tilde==1,0,logy.rhow)))
-  }
-  hat.theta[k,4:5]<-optim(hat.theta[k-1,4:5],f.rho,control=list(fnscale=-1))$par
+    return(-hat.theta[k,3]*sum((1-data1.rhow$p.tilde)*y.rhow)+sum((1-data1.rhow$p.tilde)*(delta[ind.i.u==1]-1)*logy.rhow))
+  })
+  
+  
+  hat.theta[k,5]<-optimize(f.rhow,c(0,upper.rhow),maximum = T)$maximum
   
   # Mise à jour de alpha et beta
   
+  y.u.0 <- data$obs[ind.i.u==0]-(1-Delta.P[ind.i.u==0])*data$obs.pre[ind.i.u==0]
+  y.rho  <- data$obs[ind.i.u==1]-(1-hat.theta[k,4])*data$obs.pre[ind.i.u==1]
+  y.rhow <- data$obs[ind.i.u==1]-(1+hat.theta[k,5])*data$obs.pre[ind.i.u==1]
+  logy.u.O <- ifelse(y.u.0<=0,-Inf,log(y.u.0))
+  logy.rho<-ifelse(y.rho<=0,-Inf,log(y.rho))
+  logy.rhow<-ifelse(y.rhow<=0,-Inf,log(y.rhow))
+  
+  
   f.ab <- function(x){
-    y.rho  <- obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[k,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)])
-    y.rhow <- obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1+hat.theta[k,5])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)])
-    logy.rho<-ifelse(y.rho<=0,-Inf,log(y.rho))
-    logy.rhow<-ifelse(y.rhow<=0,-Inf,log(y.rhow))
-    delta.n <- x[1]*(temps.insp^x[2]-c(0,temps.insp[1:(nb.inspections-1)])^x[2])
-    return(sum(delta.n*log(hat.theta[k,3])-log(gamma(delta.n))+p.tilde*(delta.n-1)*logy.rho+(1-p.tilde)*(delta.n-1)*ifelse(p.tilde==1,0,logy.rhow)))
+    delta.u.0 <- x[1]*(data$temps.insp[ind.i.u==0]^x[2]-data$temps.insp.pre[ind.i.u==0]^x[2])
+    delta.u.1 <- x[1]*(data$temps.insp[ind.i.u==1]^x[2]-data$temps.insp.pre[ind.i.u==1]^x[2])
+    delta.n   <- x[1]*(data$temps.insp^x[2]-data$temps.insp.pre^x[2]) # tous les delta_i
+  
+    return(sum(delta.n)*log(hat.theta[k,3])-sum(log(gamma(delta.n)))+sum((delta.u.0-1)*logy.u.O)+sum(p.tilde*(delta.u.1-1)*logy.rho+(1-p.tilde)*(delta.u.1-1)*ifelse(p.tilde==1,0,logy.rhow)))
   }
   hat.theta[k,1:2]<-optim(hat.theta[k-1,1:2],f.ab,control=list(fnscale=-1))$par
   
   
   
   # Mise à jour des delta.i
-  delta <- hat.theta[k,1]*(temps.insp^hat.theta[k,2]-c(0,temps.insp[1:(nb.inspections-1)])^hat.theta[k,2])
+  delta <- hat.theta[k,1]*(data$temps.insp^hat.theta[k,2]-data$temps.insp.pre^hat.theta[k,2])
   
   # Mise à jour des p.tilde
-  p.tilde <- (hat.theta[k,6]*dgamma(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[k,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[k,3]))/(hat.theta[k,6]*dgamma(obs-(1-c(0,Delta.P[1:(nb.inspections-1)]))*(1-hat.theta[k,4])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[k,3])+(1-hat.theta[k,6])*dgamma(obs-(1-c(0,Delta.P[(nb.inspections-1)]))*(1+hat.theta[k,5])^(c(0,vect.u[1:(nb.inspections-1)]))*c(0,obs[1:(nb.inspections-1)]),delta,rate=hat.theta[k,3]))
+  p.tilde <- ifelse(data1$obs/data1$obs.pre<=1,1,(hat.theta[k,6]*dgamma(data$obs[ind.i.u==1]-(1-hat.theta[k,4])*data$obs.pre[ind.i.u==1],delta[ind.i.u==1],rate=hat.theta[k,3]))/(hat.theta[k,6]*dgamma(data$obs[ind.i.u==1]-(1-hat.theta[k,4])*data$obs.pre[ind.i.u==1],delta[ind.i.u==1],rate=hat.theta[k,3])+(1-hat.theta[k,6])*dgamma(data$obs[ind.i.u==1]-(1+hat.theta[k,5])*data$obs.pre[ind.i.u==1],delta[ind.i.u==1],rate=hat.theta[k,3])))
+  
+  
+  data.temp <- data.frame(data1,p.tilde)
+  data1.rhow <- filter(data.temp,p.tilde<1)
+  upper.rhow <- min(data1.rhow$obs/data1.rhow$obs.pre-1)
+  
   
   print(paste("la valeur de alpha à l'étape ",k," est : ",hat.theta[k,1]))
   print(paste("la valeur de beta à l'étape ",k," est : ",hat.theta[k,2]))
@@ -195,194 +276,8 @@ for (k in 2:(K+1)){
   print("==============================================================")
 }
 
+# Identifier les p.tildes <1
+ind.poor.maintenance <- which(data1$vect.b==0)
 
-
-
-
-#####################################
-# Calcul de la loi stationnaire
-#####################################
-
-
-## update 20/05/2021
-## version discrétisée pour le calcul de la loi stationnaire
-
-# fonction ntrap : renvoie l'intégrale par la méthode des trapèzes
-ntrap <- function(abs,ord){
-  # Cette fonction renvoie un vecteur de taille nc (pour différentes valeurs de y)
-  # abscisse abs : vecteur de taille nl
-  # ordonnée ord : matrice de taille nc*nl
-  # (abs_i+1-abs_i)*(f(abs_i,ord)+f(abs_i,ord)/2
-  # (matrix(1,ncol=1,nrow=nc)%*%diff(abs)) = (x_2-x_1 x_3-x_2 ...
-  #                                           x_2-x_1 x_3-x_2 ...)
-  
-  nl<- length(abs)
-  if (is.matrix(ord)){
-    nc <- nrow(ord)
-    res <- rowSums((matrix(1,ncol=1,nrow=nc)%*%diff(abs))*(ord[,-ncol(ord)]+ord[,-1])/2)
-  } else {
-    nc <- 1
-    res <- sum(diff(abs)*(ord[-length(ord)]+ord[-1])/2)
-  }
-  return(res)
-}
-
-
-
-
-
-# vecteur de l'état x
-level.x <- seq(0.01,M+2,0.01)
-n.level.x <-length(level.x)
-
-ind.L <- which( level.x == L)
-ind.M <- which( level.x == M)
-
-
-level.y <- seq(0.01,M+2,0.01)
-n.level.y <-length(level.y)
-
-mat.trans <- matrix(0,ncol=n.level.y,nrow = n.level.x)
-mat.trans.rho <- matrix(0,ncol=n.level.y,nrow = n.level.x)
-
-for(i in 1:(n.level.x-1)){
-  mat.trans[i,(i+1):n.level.y]<-dgamma(level.y[(i+1):n.level.y]-level.x[i],scale=b,shape = alpha*tau^beta)
-  mat.trans.rho[i,(i+1):n.level.y]<-dgamma(level.y[(i+1):n.level.y]-(1-rho)*level.x[i],scale=b,shape = alpha*tau^beta)
-}
-
-K<-100
-w<-matrix(0,nrow=K,ncol=n.level.y)
-w[1,]<-dgamma(level.y,scale = b,shape=alpha*tau^beta)
-
-
-pi<-list()
-pi[[1]]<-function(x) dgamma(x,scale = b,shape=alpha*tau^beta)
-curve(pi[[1]](x), from = 0, to = max(level.x), lwd = 2, col = k)
-
-for(k in 2:K){
-  
-  # Contribution à la première intégrale :
-  
-  Q1 <- ntrap(level.x[1:ind.L],t(mat.trans[1:ind.L,]) * matrix(rep(w[k-1,1:ind.L],n.level.y),byrow = T,ncol = ind.L)) 
-  # Contribution à la deuxième intégrale :
-  
-  Q2 <- ntrap(level.x[(ind.L+1):ind.M],t(mat.trans.rho[(ind.L+1):ind.M,]) *matrix(rep(w[k-1,(ind.L+1):ind.M],n.level.y),byrow = T,ncol = ind.M-ind.L) )
-  
-  # Contribution à la troisième intégrale :
-  
-  Q3 <- dgamma(level.y,scale=b,shape=alpha*tau^beta) * ntrap(level.x[(ind.M+1):n.level.y],w[k-1,(ind.M+1):n.level.y])
-  
-  aux <- w[k,] <- (Q1+Q2+Q3)/ntrap(level.x,Q1+Q2+Q3)
-  fn_w <- function(x) {
-    res <- splinefun(x = level.x, y = aux)(x)*((0 < x) & (x<=max(level.x)))
-    return(res)
-  }
-  pi[[k]] <- fn_w
-  curve(pi[[k]](x), from = 0, to = max(level.x), lwd = 2, add = TRUE, col = k,ylab = "w_k")
-  print(paste("L'intégrale de la", k,"-ième fonction vaut",integrate(fn_w,0,Inf)$value,sep=" "))
-  #  scan()
-}
-######################
-###   fin update   ###
-###################### 
-
-
-# 1ere intégrale \int_0^L \int_L^M f(y-x) pi(dx)
-
-#PI <- Vectorize(function(u) integrate(pi[[K]],0,u)$value)
-
-PI <- Vectorize(function(u,n=10000) {
-  if (u>max(level.x)) {
-    res<-1
-  }
-  else {
-    #set.seed(123)
-    x<-runif(n,0,u)
-    res <- mean(pi[[K]](x))*u
-  }  
-  return(res)
-})
-
-
-
-# Tirer selon loi stationnaire :
-
-# Tirer un u selon U[0,1]
-samplePi.aux <- function() {
-  uu<-runif(1)
-  uniroot(function(u) PI(u) - uu,c(0,10000))$root
-}
-
-
-
-ech<-replicate(10000,samplePi.aux())
-yPi <- runif(10000,L,M)
-mean(dgamma(yPi-ech,scale=b,shape=alpha*tau^beta)*(M-L))*PI(M)
-
-
-# Ne semble pas fonctionner quand on compare l'histo avec la densité ! 23/06/2021.
-
-yPi <- runif(100000,L,M)
-xPi <- runif(100000,0,M)
-
-mean(dgamma(yPi-xPi,scale=b,shape=alpha*tau^beta)*pi[[K]](xPi))*(M-L)*M
-
-
-# A re
-
-xPi <- runif(100000,0,L)
-yPi <- runif(100000,0,M)
-1-mean(dgamma(yPi-xPi,scale=b,shape=alpha*tau^beta)*pi[[K]](xPi))*(M)*L
-
-# Peut-être choisir une méthode plus efficace... car tous les x>y ne contribuent pas à l'intégrale
-
-
-K<- 20 # nb itérations de l'algo de point fixe
-grid_abs <- seq(0.01,10,0.01)
-nb_abs <- length(grid_abs)
-w<-matrix(nrow = K,ncol = nb_abs )
-
-pi<-list()
-pi[[1]]<-function(x) dgamma(x,scale = b,shape=alpha*tau^beta)
-
-
-curve(pi[[1]](x), from = 0, to = max(grid_abs), lwd = 2)
-
-for(k in 2:K){
-  for (j in 1:nb_abs){
-    abs <- grid_abs[j]
-    fn_aux1 <- function(x) {
-      res <- pi[[k-1]](x)*dgamma(abs-x,scale = b,shape=alpha*tau^beta)
-      return(res)
-    }
-    fn_aux2 <- function(x) {
-      res <- pi[[k-1]](x)*dgamma(abs-(1-rho)*x,scale=b,shape=alpha*tau^beta)
-      return(res)
-    }
-    fn_aux3 <- function(x) {
-      res <- pi[[k-1]](x)
-      return(res)
-    }
-    w[k,j] <- integrate(f = fn_aux1, lower = 0, upper = min(L,abs))$value 
-    + ifelse(min(abs/(1-rho),L)==min(abs/(1-rho),M),0,integrate(f = fn_aux2, lower = min(abs/(1-rho),L), upper = min(abs/(1-rho),M))$value)
-    + integrate(f = fn_aux3, lower = M, upper = Inf)$value * dgamma(abs,scale=b,shape=alpha*tau^beta)
-  }
-  aux <- w[k,]
-  fn_w <- function(x) {
-    res <- splinefun(x = grid_abs, y = aux)(x)*((0 < x) & (x<=max(grid_abs)))
-    return(res)
-  }
-  pi[[k]] <- fn_w
-  curve(pi[[k]](x), from = 0, to = max(grid_abs), lwd = 2, add = TRUE, col = k)
-  print(paste("L'intégrale de la", k,"-ième fonction vaut",integrate(fn_w,0,Inf)$value,sep=" "))
-}
-
-
-
-
-
-
-
-
-
-
+# Maintenance où yappartient à [L,M]
+cbind(temps.insp[ind.u],vect.b[ind.u],1-p.tilde)

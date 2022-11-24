21ed69319170e4b1a43c60bb921c1638e1bfb69c
fcorset <franck.corset@univ-grenoble-alpes.fr>
2022-11-24 09:14:41 +0100

    Traitement.R

--- ENBIS/Prog/Sept2022/traitement.R
+++ ENBIS/Prog/Sept2022/traitement.R
@@ -1,5 +1,24 @@
+rm(list=ls())
+library(ggplot2)
+library(tidyverse)
+library(stringr)
+library(dplyr)
 # Charger le fichier de données
 # *.Rdata
+alpha <- 1
+beta <- 1
+b <- 1
+rho <- 0.8
+rho_w <- 0.5
+p <- 0.9
+HT <- 500
+theta<-c(alpha,beta,b,rho,rho_w,p)
+txt <- str_remove_all(paste("./Results/estim_",alpha,"_",beta,"_",b,"_",rho,"_",rho_w,"_",p,"_",HT)," ")
+load(str_remove_all(paste(txt,".Rdata")," "))
+
+
+
+
 # Donner les vraies valeurs des paramètres
 label.theta <-c("alpha","beta","b","rho","rho_w","p")
 #theta <- c(alpha,beta,b,rho,rho_w,p)

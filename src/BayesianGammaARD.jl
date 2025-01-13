a8c2962e33559e8b158cd81b98cb83d89e1f9ee5
fcorset <franck.corset@gmail.com>
2025-01-13 13:17:22 +0100

    MLE

--- src/BayesianGammaARD.jl
+++ src/BayesianGammaARD.jl
@@ -1,5 +1,14 @@
 module BayesianGammaARD
+using Distributions
+using DataFrames
+using SpecialFunctions
+using Optim
+using Statistics
+using CSV
 
+
+export GammaProcess, MaintenanceModel, MLE, loglikelihood
 # Write your package code here.
-
+include("maintenancemodels.jl")
+include("gammaprocesses.jl")
 end
